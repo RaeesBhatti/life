65e0a826368d65e79cf8f5370da3b50d1dd0c953
Raees Iqbal <raees.bhatti@gmail.com>
2016-04-27 06:19:13 +0500

    Errors on invalid input

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', (e) => {
+
 	const dobInput = document.getElementById('dob')
 	const usage = document.getElementById('usage')
-	const calcBtn = document.getElementById('calc-btn')
-	if(dobInput && dobInput instanceof HTMLInputElement && usage && usage instanceof HTMLParagraphElement &&
-		calcBtn && calcBtn instanceof HTMLButtonElement){
+	const inputs = document.getElementById('inputs')
+
+	if(inputs instanceof HTMLFormElement && dobInput instanceof HTMLInputElement && usage instanceof HTMLParagraphElement){
 
 		let max = new Date()
 				max.setYear(max.getYear() - 5)
@@ -17,56 +18,48 @@ document.addEventListener('DOMContentLoaded', (e) => {
 		dobInput.min = (min.getYear() + 1900) - 90 + '-' +
 				(min.getMonth().length > 0 ? min.getMonth() : '0' + min.getMonth()) + '-' + max.getDate()
 
-		calcBtn.addEventListener('click', (e) => {
-			if(!dobInput.validity.valid){
-				dobInput.focus()
-			} else {
-				let dob = new Date(dobInput.valueAsDate)
-				let maxLife = new Date(dobInput.valueAsDate)
-						maxLife.setYear(maxLife.getYear() + 90 + 1900)
-				let weeks = Math.round(Math.round((today - dob) / 8.64e7) / 7);
+		inputs.addEventListener('submit', (e) => {
+			e.preventDefault()
 
-				while(calender.firstChild){
-					calender.removeChild(calender.firstChild)
-				}
-				for(i = 0; i <= 4693; ++i) {
-					let span = document.createElement('span')
+			let dob = new Date(dobInput.valueAsDate)
+			let maxLife = new Date(dobInput.valueAsDate)
+			maxLife.setYear(maxLife.getYear() + 90 + 1900)
+			let weeks = Math.round(Math.round((today - dob) / 8.64e7) / 7);
 
-					switch (true) {
+			while(calender.firstChild){
+				calender.removeChild(calender.firstChild)
+			}
+			for(i = 0; i <= 4693; ++i) {
+				let span = document.createElement('span')
+
+				switch (true) {
 						// already spent weeks
-						case weeks > i:
-							span.classList.add('black')
-							break
+					case weeks > i:
+						span.classList.add('black')
+						break
 
 						// first 7 years
-						case i < 365:
-							span.classList.add('black')
-							break
+					case i < 365:
+						span.classList.add('black')
+						break
 
 						// 12 years
-						case i < 626:
-							span.classList.add('blue')
-							break
+					case i < 626:
+						span.classList.add('blue')
+						break
 
 						// more than 85 years
-						case i > 4432 :
-							span.classList.add('red')
-							break
+					case i > 4432 :
+						span.classList.add('red')
+						break
 
-						default:
-					}
-					calender.appendChild(span)
+					default:
 				}
-
-				while(usage.firstChild){
-					usage.removeChild(usage.firstChild)
-				}
-				let p = document.createElement('p')
-				p.innerHTML = 'You have used <strong>' + ((((today.getYear() + 1900) - (dob.getYear() + 1900)) / 90) * 100).toFixed(3)
-						+ '%</strong> of your life'
-				usage.appendChild(p)
-
+				calender.appendChild(span)
 			}
+
+			usage.innerHTML = '<p>You have used <strong>' +
+					((((today.getYear() + 1900) - (dob.getYear() + 1900)) / 90) * 100).toFixed(3) + '%</strong> of your life</p>'
 		})
 	}
 
